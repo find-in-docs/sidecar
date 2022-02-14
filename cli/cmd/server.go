7e2c32532b4fd15b23a114b0daed59d8a17b205e
samirgadkari <samirgadkari@users.noreply.github.com>
2022-02-14 15:09:57 -0800

    Server method 'Register' created

--- cli/cmd/server.go
+++ cli/cmd/server.go
@@ -5,22 +5,42 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"os"
 	"sync"
 
 	"github.com/nats-io/nats.go"
 	"github.com/samirgadkari/sidecar/pkg/connection"
 	"github.com/samirgadkari/sidecar/pkg/connection/config"
+	"github.com/samirgadkari/sidecar/protos/v1/messages"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"google.golang.org/grpc"
 )
+
+type server struct {
+	messages.UnimplementedSidecarServer
+}
+
+func (s *server) Register(ctx context.Context, in *messages.RegistrationMsg) (*messages.RegistrationResponse, error) {
+	fmt.Printf("Received RegistrationMsg: %v\n", in)
+
+	header := messages.ResponseHeader{
+		Status: uint32(messages.Status_OK),
+	}
+	return &messages.RegistrationResponse{
+		Header: &header,
+		Msg:    "OK",
+	}, nil
+}
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
 	Use:   "server",
-	Short: "Start a server to talk to a NATS client.",
-	Long: `Start a server to talk to a NATS client. 
+	Short: "Start a server to talk to a NATS client and listen to GRPC requests.",
+	Long: `Start a server to talk to a NATS client and listen to GRPC requests. 
 The server will connect, and run some tests between two sidecar instances`,
 	Run: func(cmd *cobra.Command, args []string) {
 
@@ -32,6 +52,24 @@ The server will connect, and run some tests between two sidecar instances`,
 			fmt.Printf("Error connecting to NATS server: %v\n", err)
 			os.Exit(-1)
 		}
+
+		go func() {
+			sidecarServiceAddr := viper.GetString("sidecarServiceAddr")
+
+			lis, err := net.Listen("tcp", sidecarServiceAddr)
+			if err != nil {
+				fmt.Printf("Error starting net listener\n\terr: %v\n", err)
+				os.Exit(-1)
+			}
+
+			s := grpc.NewServer()
+			messages.RegisterSidecarServer(s, &server{})
+
+			fmt.Printf("Server listening at %v", lis.Addr())
+			if err = s.Serve(lis); err != nil {
+				fmt.Print("Failed to serve: %v\n", err)
+			}
+		}()
 
 		var wg sync.WaitGroup
 		numMsgs := 10
